eb8fde09314868569031443fade47a7c949ba73d
nueldeli <nueljoel34@gmail.com>
2022-04-08 16:46:57 +0800

    figure.app updating models.py

--- figure/models.py
+++ figure/models.py
@@ -27,6 +27,22 @@ class Division(models.Model):
 class PlantingFigure(models.Model):
 	date_input = models.DateTimeField(auto_now_add=True)
 	date_planted = models.CharField(max_length=50)
-	program_name = models.CharField(max_length=200)
+	planting_program_name = models.CharField(max_length=200)
 	planting_division = models.ForeignKey(Division, on_delete=models.CASCADE, null=True)
-	
+	planting_total_trees_planted = models.IntegerField()
+	planting_species = models.TextField()
+	planting_number_of_species = models.IntegerField()
+	planting_total_hectares = models.FloatField()
+	planting_location = models.CharField(max_length=250)
+
+	class Meta:
+		ordering = ['-planting_total_trees_planted']
+
+	def __str__(self):
+		return self.planting_program_name
+
+	def get_absolute_url(self):
+		return reverse_lazy('figure_index')
+
+
+
