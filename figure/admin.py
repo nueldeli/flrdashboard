d697f54714bce585f11408c6e00ca00a472d4caa
nueldeli <nueljoel34@gmail.com>
2022-04-08 16:55:56 +0800

    figure.app updated admin.py

--- figure/admin.py
+++ figure/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import FigureOverview, Division, PlantingFigure
 
 # Register your models here.
+class FigureOverviewAdmin(admin.ModelAdmin):
+	list_display = ('total_trees_planted', 'total_species', 'total_hectares')
+
+class DivisionAdmin(admin.ModelAdmin):
+	list_display = ('division_name')
+
+class PlantingFigureAdmin(admin.ModelAdmin):
+	list_display = ('planting_program_name', 'planting_division')
+
+admin.site.register(FigureOverview, FigureOverviewAdmin)
+admin.site.register(Division, DivisionAdmin)
+admin.site.register(PlantingFigure, PlantingFigureAdmin)
